c99988e957af03be12a96ef9c698f95e2a0c7785
Jhovany Rodríguez <jhovany50@gmail.com>
2026-08-08 07:04:02 -0600

    refactor: improve database configuration

--- src/config/db.js
+++ src/config/db.js
@@ -3,19 +3,21 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const pool = process.env.DATABASE_URL
-  ? new Pool({
+const dbConfig = process.env.DATABASE_URL
+  ? {
       connectionString: process.env.DATABASE_URL,
       ssl: {
-        rejectUnauthorized: false,
-      },
-    })
-  : new Pool({
-      user: process.env.DB_USER,
+        rejectUnauthorized: false
+      }
+    }
+  : {
       host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
-    });
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    };
+
+const pool = new Pool(dbConfig);
 
 export default pool;
